cbecc44c71265199d4cb9cd61e4925c3a6d39048
Kevin Kim <kevinfkim@gmail.com>
2020-07-23 16:19:47 -0400

    admin/category page setup.

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,16 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+  end
+end
