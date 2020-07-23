d05340d9b634f7c461b29e1297bbf139726bdfe6
Kevin Kim <kevinfkim@gmail.com>
2020-07-22 23:34:44 -0400

    dashboard creation with two cards with quantities. link for category still to be created.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.all.order(created_at: :desc)
+    @category = Category.all.order(created_at: :desc)
+
   end
 end
